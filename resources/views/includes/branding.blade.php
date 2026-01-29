8efeb32eeeda122823b6f878670400ce06990b0a
Mahabubul Hasan <codehasan@gmail.com>
2026-01-29 11:43:46 +0600

    Add branding component

---
+++ resources/views/includes/branding.blade.php
@@ -0,0 +1,12 @@
+<div style="display: flex; justify-content: space-between; align-items: center;">
+    <div style="display: flex; gap: 10px;">
+        <div><img src="{{ asset('img/stl/DPHE-small.jpg') }}" alt="DPHE" style="height: 50px;"></div>
+        <div><img src="{{ asset('img/stl/Gates.png') }}" alt="Gates Foundation" style="height: 50px;"></div>
+        <div><img src="{{ asset('img/stl/IsDB.png') }}" alt="IsDB" style="height: 50px;"></div>
+    </div>
+    <div style="display: flex; gap: 10px;">
+        <div><img src="{{ asset('img/stl/GWSC.png') }}" alt="GWSC" style="height: 50px;"></div>
+        <div><img src="{{ asset('img/stl/STL.png') }}" alt="STL" style="height: 50px;"></div>
+        <div><img src="{{ asset('img/stl/ispl.png') }}" alt="ISPL" style="height: 50px;"></div>
+    </div>
+</div>
