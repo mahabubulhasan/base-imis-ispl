7ba2fa6f3e55e818701ecce46d1a6e16c4ea1a77
Mahabubul Hasan <codehasan@gmail.com>
2026-01-30 16:30:56 +0600

    Fix public dashboard route definition to use class reference syntax

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('/', function () {
     }
 });
 
-Route::get('/public-dashboard', [App\Http\Controllers\PublicDashboardController::class, 'index'])->name('public-dashboard');
+Route::get('/public-dashboard', 'PublicDashboardController@index')->name('public-dashboard');
 
 Route::get('/fsm-application', 'Fsm\PublicApplicationController@getForm')->name('client-fsm-application.form');
 Route::post('/fsm-application', 'Fsm\PublicApplicationController@submitForm')->name('client-fsm-application.submit');
