81b876d587bfb96115f051fa39600bb07b23a2d1
Mahabubul Hasan <codehasan@gmail.com>
2026-02-05 12:05:02 +0600

    Update footer copyright information to reflect new licensing and company details

--- resources/views/maps/index.blade.php
+++ resources/views/maps/index.blade.php
@@ -1328,8 +1328,8 @@ Developed By: Innovative Solution Pvt. Ltd. (ISPL)   -->
         <div class="float-right d-none d-sm-inline ">
         	<strong>Implemented by:</strong> <a href="https://streamstech.com">Streams Tech Ltd.</a>
     	</div>
-        <strong> Base IMIS <i class="fa-regular fa-copyright"> </i>  2022-{{ \Carbon\Carbon::now()->format('Y') }} by <a href="https://streamstech.com">
-    Streams Tech Ltd.</a> & <a href="https://www.gwsc.ait.ac.th/">Global Water & Sanitation Center-Asian Institute of Technology (GWSC-AIT)</a> is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1">CC BY-NC-SA 4.0 </a>
+            <strong> Base IMIS <i class="fa-regular fa-copyright"> </i>  2022-{{ \Carbon\Carbon::now()->format('Y') }} by <a href="http://www.innovativesolution.com.np">
+	ISPL</a> & <a href="https://www.gwsc.ait.ac.th/">GWSC-AIT</a> is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1">CC BY-NC-SA 4.0 </a>
 </strong>
 
         <!-- Default to the left -->
