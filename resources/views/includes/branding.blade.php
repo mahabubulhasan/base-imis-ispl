c3b5458acece00c33be78c5d90638d2eb2dc060a
ahamed-kashif <ahamed.kashif@northsouth.edu>
2026-04-28 13:40:20 +0600

    logos realigned

--- resources/views/includes/branding.blade.php
+++ resources/views/includes/branding.blade.php
@@ -3,13 +3,13 @@ Developed By: Streams Tech Ltd.
 Description: Footer branding logos layout for partner and implementer organizations -->
 <div style="display: flex; justify-content: space-between; align-items: center;">
     <div style="display: flex; gap: 10px; align-items: center;">
-        <div><img src="{{ asset('img/stl/SNV_logo.svg') }}" alt="SNV" style="height: 60px;"></div>
-        <div><img src="{{ asset('img/stl/kingdom-of-the-netherlands-logo-png_seeklogo.png') }}" alt="Kingdom of the Netherlands" style="height: 50px;"></div>
-        <div><img src="{{ asset('img/stl/logo-chapainawabganj.png') }}" alt="Chapainawabganj Paurashava" style="height: 50px;"></div>
+        <div><img src="{{ asset('img/stl/logo-chapainawabganj.png') }}" alt="Chapainawabganj Paurashava" style="height: 70px;"></div>
+        <div><img src="{{ asset('img/stl/kingdom-of-the-netherlands-logo-png_seeklogo.png') }}" alt="Kingdom of the Netherlands" style="height: 70px;"></div>
     </div>
     <div style="display: flex; gap: 10px; align-items: center;">
         {{-- <div><img src="{{ asset('img/stl/GWSC.png') }}" alt="GWSC" style="height: 50px;"></div>
         <div><img src="{{ asset('img/stl/ispl.png') }}" alt="ISPL" style="height: 50px;"></div> --}}
-        <div><img src="{{ asset('img/stl/STL.png') }}" alt="STL" style="height: 50px;"></div>
+        <div><img src="{{ asset('img/stl/SNV_logo.svg') }}" alt="SNV" style="height: 70px;"></div>
+        <div><img src="{{ asset('img/stl/STL.png') }}" alt="STL" style="height: 70px;"></div>
     </div>
 </div>
