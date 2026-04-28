8fa10a0266e463a45f88a545aaffe12dfc110583
ahamed-kashif <ahamed.kashif@northsouth.edu>
2026-04-28 19:59:48 +0600

    sts edit issue fixed

--- routes/web.php
+++ routes/web.php
@@ -306,7 +306,9 @@ Route::group([
         Route::get('sts/data', 'StsController@getData')->name('swm.sts.data');
         Route::get('sts/export', 'StsController@export')->name('swm.sts.export');
         Route::get('sts/{sts}/history', 'StsController@history')->name('swm.sts.history');
-        Route::resource('sts', 'StsController')->names([
+        Route::resource('sts', 'StsController')->parameters([
+            'sts' => 'sts',
+        ])->names([
             'index' => 'swm.sts.index',
             'create' => 'swm.sts.create',
             'store' => 'swm.sts.store',
