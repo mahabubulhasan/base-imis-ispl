671acf79c714a4d46a7086163fc890dc5b1a9ecb
sahkashif <kasheef730@gmail.com>
2026-06-16 11:20:50 +0600

    footer logo issue in desktop view fixed

--- resources/views/includes/branding.blade.php
+++ resources/views/includes/branding.blade.php
@@ -2,16 +2,20 @@
 Developed By: Streams Tech Ltd.
 Description: Footer branding logos layout for partner and implementer organizations -->
 <div class="footer-branding">
-    <div class="footer-branding__logo">
-        <img src="{{ asset('img/stl/logo-chapainawabganj.png') }}" alt="Chapainawabganj Paurashava">
+    <div class="footer-branding__group">
+        <div class="footer-branding__logo">
+            <img src="{{ asset('img/stl/logo-chapainawabganj.png') }}" alt="Chapainawabganj Paurashava">
+        </div>
+        <div class="footer-branding__logo">
+            <img src="{{ asset('img/stl/kingdom-of-the-netherlands-logo-png_seeklogo.png') }}" alt="Kingdom of the Netherlands">
+        </div>
     </div>
-    <div class="footer-branding__logo">
-        <img src="{{ asset('img/stl/kingdom-of-the-netherlands-logo-png_seeklogo.png') }}" alt="Kingdom of the Netherlands">
-    </div>
-    <div class="footer-branding__logo">
-        <img src="{{ asset('img/stl/SNV_logo.svg') }}" alt="SNV">
-    </div>
-    <div class="footer-branding__logo">
-        <img src="{{ asset('img/stl/STL.png') }}" alt="STL">
+    <div class="footer-branding__group">
+        <div class="footer-branding__logo">
+            <img src="{{ asset('img/stl/SNV_logo.svg') }}" alt="SNV">
+        </div>
+        <div class="footer-branding__logo">
+            <img src="{{ asset('img/stl/STL.png') }}" alt="STL">
+        </div>
     </div>
 </div>
