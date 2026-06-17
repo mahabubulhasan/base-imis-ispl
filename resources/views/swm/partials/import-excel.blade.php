291df66f32dd8b1c59c02a78a2b919889a2e7f16
sahkashif <kasheef730@gmail.com>
2026-06-17 19:03:11 +0600

    excel import page back to list btn made smaller

--- resources/views/swm/partials/import-excel.blade.php
+++ resources/views/swm/partials/import-excel.blade.php
@@ -5,23 +5,19 @@
 @include('layouts.components.success-alert')
 @include('layouts.components.error-alert')
 <div class="card card-info">
-    <div class="card-header">
-        <a href="{{ $backRoute }}" class="btn btn-info">{{ __('Back to List') }}</a>
-    </div>
-    <div class="app-mobile-form">
-    <div class="card-body">
-        {!! Form::open(['route' => $storeRoute, 'files' => true, 'class' => 'form-horizontal']) !!}
-        <div class="form-group row required">
-            {!! Form::label('import_file', __('Excel file (.xlsx)'), ['class' => 'col-sm-3 control-label']) !!}
-            <div class="col-sm-6">
-                {!! Form::file('import_file', ['class' => 'form-control', 'accept' => '.xlsx']) !!}
+    {!! Form::open(['route' => $storeRoute, 'files' => true, 'class' => 'form-horizontal']) !!}
+        <div class="card-body">
+            <div class="form-group row required">
+                {!! Form::label('import_file', __('Excel file (.xlsx)'), ['class' => 'col-sm-3 control-label', 'style' => 'padding-top:3px;']) !!}
+                <div class="col-sm-3">
+                    {!! Form::file('import_file', ['accept' => '.xlsx']) !!}
+                </div>
             </div>
         </div>
         <div class="card-footer">
+            <a href="{{ $backRoute }}" class="btn btn-info">{{ __('Back to List') }}</a>
             {!! Form::submit(__('Import'), ['class' => 'btn btn-info']) !!}
         </div>
-        {!! Form::close() !!}
-    </div>
-    </div>
+    {!! Form::close() !!}
 </div>
 @stop
