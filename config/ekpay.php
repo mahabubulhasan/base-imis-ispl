6e87b78774a9f13e5a35d4e9a9a5a56740e5d17c
Mahabubul Hasan <codehasan@gmail.com>
2026-07-02 13:25:30 +0600

    fix (ekpay): correct mac address in configuration

--- config/ekpay.php
+++ config/ekpay.php
@@ -4,6 +4,5 @@ return [
     'MERCHANT_REG_ID' => env('MERCHANT_REG_ID', 'lakshmipur_pouro_imis'),
     'MERCHANT_PAS_KEY' => env('MERCHANT_PAS_KEY', 'dC8#N7H?'),
     'SANDBOX_ENABLED' => env('SANDBOX_ENABLED', false),
-    // 'MAC' => env('MAC', '1.1.1.1')
-    'MAC' => env('MAC', '13.228.125.1')
+    'MAC' => env('MAC', '1.1.1.1')
 ];
