779d32194fb585753f0d3c2fe66c840f10afe9a8
Mahabubul Hasan <codehasan@gmail.com>
2026-07-02 13:24:26 +0600

    fix(ekpay): update sandbox configuration and mac address

--- config/ekpay.php
+++ config/ekpay.php
@@ -3,6 +3,7 @@
 return [
     'MERCHANT_REG_ID' => env('MERCHANT_REG_ID', 'lakshmipur_pouro_imis'),
     'MERCHANT_PAS_KEY' => env('MERCHANT_PAS_KEY', 'dC8#N7H?'),
-    'SANDBOX_ENABLED' => env('SANDBOX_ENABLED', true),
-    'MAC' => env('MAC', '1.1.1.1')
+    'SANDBOX_ENABLED' => env('SANDBOX_ENABLED', false),
+    // 'MAC' => env('MAC', '1.1.1.1')
+    'MAC' => env('MAC', '13.228.125.1')
 ];
