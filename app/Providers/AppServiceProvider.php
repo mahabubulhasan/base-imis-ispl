ee50be82e4290ec45785e1013dcee7e15fa9108c
Mahabubul Hasan <codehasan@gmail.com>
2026-07-06 16:03:14 +0600

    feat (payment): update IPN URI to correct endpoint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
                 ->set(ConfigKey::MERCHANT_REG_ID, config('ekpay.MERCHANT_REG_ID'))
                 ->set(ConfigKey::MERCHANT_PAS_KEY, config('ekpay.MERCHANT_PAS_KEY'))
                 ->set(ConfigKey::IPN_CHANNEL, '1') // 0=None, 1=Both, 2=Email, 3=API
-                ->set(ConfigKey::IPN_URI, url("/api/ipn"))
+                ->set(ConfigKey::IPN_URI, url("/payment/ipn"))
                 ->set(ConfigKey::IPN_EMAIL, config('ekpay.IPN_EMAIL'))
                 ->set(ConfigKey::MAC, config('ekpay.MAC'))
                 ->set(Configkey::SANDBOX_ENABLED, config('ekpay.SANDBOX_ENABLED'));
