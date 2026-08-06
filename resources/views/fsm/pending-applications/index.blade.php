71c979c5421d543e0ae7cb6a9bada947a918e1ff
Mahabubul Hasan <codehasan@gmail.com>
2026-08-06 11:10:35 +0600

    Update language translation key

--- resources/views/fsm/pending-applications/index.blade.php
+++ resources/views/fsm/pending-applications/index.blade.php
@@ -49,9 +49,9 @@
                                             <div class="col-md-4">
                                                 <input type="text" id="ward" class="form-control" placeholder="{{ __('Ward') }}">
                                             </div>
-                                            <label class="col-md-2 col-form-label">{{ __('Applicant Contact') }}</label>
+                                            <label class="col-md-2 col-form-label">{{ __('Customer Contact') }}</label>
                                             <div class="col-md-4">
-                                                <input type="text" id="customer_contact" class="form-control" placeholder="{{ __('Applicant Contact') }}">
+                                                <input type="text" id="customer_contact" class="form-control" placeholder="{{ __('Customer Contact') }}">
                                             </div>
                                         </div>
                                         <div class="card-footer text-right">
